a6eb39682dd6e110a5c432bd1b206c475815028b
Paulo Benetello <paulohbenetello@gmail.com>
2026-05-12 13:30:52 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell())
+            {
+                Width = 400,
+                Height = 600
+            };
+        }
+     
+    }
+}
